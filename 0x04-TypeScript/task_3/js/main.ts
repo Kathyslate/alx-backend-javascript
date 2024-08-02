4d8c79372baa6814db2d771fa696eef4632150a4
kathyslate <mercyoyetunde98@gmail.com>
2024-08-02 14:45:28 -0700

    task 3

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,87 +1,15 @@
-export interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
-  location: string;
+
+import { RowID, RowElement } from './interface';
+import CRUD from './crud';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
 }
-const studentA: Student = {
-  firstName: "mercy",
-  lastName: "oyetunde",
-  age: 29,
-  location: "osun state",
-};
-const studentB: Student = {
-  firstName: "ibukun",
-  lastName: "olaidee",
-  age: 29,
-  location: "ogun state",
-};
 
-const studentsList: Array<Student> = [
-  studentA,
-  studentB,
-];
-const styleSheet = `
-  html {
-    margin: 0;
-    height: 100%;
-  }
-  body {
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 80%;
-    margin: 10%;
-  }
-  table {
-    border-collapse: collapse;
-  }
-  thead {
-    font-weight: bold;
-  }
-  td {
-    padding: 10px;
-    border: 1px solid gray;
-    cursor: pointer;
-  }
-  td:hover {
-    background: gainsboro;
-  }
+const newRowID: RowID = CRUD.insertRow(row);
+row.age = 23;
+const updatedRow: RowElement = row;
 
-  td:nth-child(1) {
-    text-align: center;
-  }
-`;
-
-/**
- * Displays information about students in a table.
- * @param students The list of students to display.
- * @author Mercy Oyetunde <https://github.com/kathyslate>
- */
-export const displayStudents = (students: Array<Student>): void => {
-  const table = document.createElement('table');
-  const tableHead = document.createElement('thead');
-  const headRow = document.createElement('tr');
-  const tableBody = document.createElement('tbody');
-  headRow.insertAdjacentHTML('beforeend', '<td>FirstName</td');
-  headRow.insertAdjacentHTML('beforeend', '<td>Location</td');
-  tableHead.insertAdjacentElement('beforeend', headRow);
-
-  for (const student of students) {
-    const bodyRow = document.createElement('tr');
-    bodyRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
-    bodyRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
-    tableBody.insertAdjacentElement('beforeend', bodyRow);
-  }
-
-  table.insertAdjacentElement('beforeend', tableHead);
-  table.insertAdjacentElement('beforeend', tableBody);
-  document.body.insertAdjacentElement('beforeend', table);
-};
-
-displayStudents(studentsList);
-const styleSheetElement = document.createElement('style');
-styleSheetElement.innerHTML = styleSheet;
-document.head.insertAdjacentElement('beforeend', styleSheetElement);
-document.title = 'Task 0';
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
